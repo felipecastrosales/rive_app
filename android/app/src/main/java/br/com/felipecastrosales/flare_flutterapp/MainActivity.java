e6bd6d51f7f3b1ce115a5aebf236b976da9127a3
Felipe Sales <soufeliposales@gmail.com>
2024-07-09 14:25:03 -0300

    build: updated android build for newer flutter versions

--- android/app/src/main/java/br/com/felipecastrosales/flare_flutterapp/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package br.com.felipecastrosales.rive_app;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ android/app/src/main/kotlin/com/example/rive_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rive_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
